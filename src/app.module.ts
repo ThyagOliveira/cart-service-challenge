12c0f529a9fe168715bac38d1d5e910c80a2c48c
ThyagOliveira <taothyago@gmail.com>
2023-05-22 20:25:20 +0100

    :gem: Add carts services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CartsModule } from './carts/carts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from './config/config.typeorm.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    CartsModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+      inject: [TypeOrmConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/carts/carts.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { CartsService } from './carts.service';
+import { CartsController } from './carts.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CartEntity } from './entities/cart.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([CartEntity])],
+  controllers: [CartsController],
+  providers: [CartsService],
+  exports: [CartsService],
+})
+export class CartsModule {}
